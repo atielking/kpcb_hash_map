d2969c4b766bfa815da43b77de145879aa2fa242
Allison <atielkin@stanford.edu>
2017-09-20 07:47:02 -0700

    finished test methods

--- HashMapTest.py
+++ HashMapTest.py
@@ -11,8 +11,57 @@ class HashMapTest(unittest.TestCase):
     def testSizeNotInt(self):
         self.assertRaises(TypeError, HashMap, "Uh-Oh")
 
-    def testSetElement(self):
+    def testSetNewElement(self):
+        hm = HashMap(10)
+        self.assertTrue(hm.set("keyTest", "valTest"))
 
-    def testFull(self):
+    def testSetOverwriteElement(self):
+        hm = HashMap(10)
+        h.set("keyTest1," "valTest1")
+        self.assertTrue(h.set("keyTest1", "valTest3"))
+
+    def testSetFull(self):
+        hm = HashMap(2)
+        h.set("keyTest1," "valTest1")
+        h.set("keyTest2", "valTest2")
+        self.assertFalse(h.set("keyTest3", "valTest3"))
+
+    def testGetElementInMap(self):
+        hm = HashMap(10)
+        hm.set("keyTest1", "valTest1")
+        self.assertIsNotNone(hm.get("keyTest1"))
+
+    def testGetElementNotInMap(self):
+        hm = HashMap(10)
+        self.assertIsNone(hm.get("keyTest1"))
+
+    def testKeyHasValue(self):
+        hm = HashMap(10)
+        hm.set("keyTest1", "valTest1")
+        self.assertIs(hm.get("keyTest1", "valTest1"))
+
+    def testValueOverwritten(self):
+        hm = HashMap(10)
+        hm.set("keyTest1", "valTest1")
+        hm.set("keyTest1", "valTest2")
+        self.assertIs(hm.get("keyTest1", "valTest2"))
+
+    def testValueDeletedNotNull(self):
+        hm = HashMap(10)
+        hm.set("keyTest1", "valTest1")
+        hm.delete("keyTest1")
+        self.assertIsNone(hm.get("keyTest1"))
+
+    def testDeleteReturnVal(self):
+        hm = HashMap(10)
+        hm.set("keyTest1", "valTest1")
+        self.assertIs(hm.delete("keyTest1"), "valTest1")
+
+    def testValueReturnNull(self):
+        hm = HashMap(10)
+        self.assertIsNone(hm.delete("keyTest1"))
 
     def testLoad(self):
+        hm = HashMap(10)
+        hm.set("keyTest1", "valTest1")
+        self.assertEqual(hm.load(), 0.1)
